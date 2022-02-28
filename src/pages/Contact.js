bb71d359e41673559339c808b26599303a3b8982
vinaykumar785557 <c0785557@mylambton.ca>
2022-02-28 00:13:13 -0500

    contact form page is created and form inputs data is handled using useRef hook

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,52 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import '../styles/Contact.css';
+import photo from '../assets/photo.jpeg';
 function Contact() {
-	return <div>Contact</div>;
+	const nameInputRef = useRef();
+	const emailInputRef = useRef();
+	const messageInputRef = useRef();
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const name = nameInputRef.current.value;
+		const email = emailInputRef.current.value;
+		const message = messageInputRef.current.value;
+
+		const userEnteredData = { name, email, message };
+
+		console.log(userEnteredData);
+
+		nameInputRef.current.value = '';
+		emailInputRef.current.value = '';
+		messageInputRef.current.value = '';
+	};
+
+	return (
+		<div className='contact'>
+			<div
+				className='leftSide'
+				style={{ backgroundImage: `url(${photo})` }}></div>
+			<div className='rightSide'>
+				<h1> Contact Us</h1>
+				<form onSubmit={submitHandler}>
+					<label htmlFor='name'>Name</label>
+					<input name='name' type='text' ref={nameInputRef} />
+					<label htmlFor='email' value=''>
+						Email
+					</label>
+					<input name='email' type='email' ref={emailInputRef} />
+					<label htmlFor='message'>Message</label>
+					<textarea
+						rows='6'
+						name='message'
+						required
+						ref={messageInputRef}></textarea>
+					<button type='submit'> Send Message</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
