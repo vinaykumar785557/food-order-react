37de284c2f32f7b8a5beb7289a61f75eab6a906d
vinaykumar785557 <c0785557@mylambton.ca>
2022-02-27 22:32:15 -0500

    menu about and contact pages with routes have been created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
 
 function App() {
 	return (
@@ -11,6 +14,15 @@ function App() {
 				<Navbar />
 				<Switch>
 					<Route path='/' exact component={Home}></Route>
+					<Route path='/menu'>
+						<Menu />
+					</Route>
+					<Route path='/about'>
+						<About />
+					</Route>
+					<Route path='/contact'>
+						<Contact />
+					</Route>
 				</Switch>
 				<Footer />
 			</Router>
